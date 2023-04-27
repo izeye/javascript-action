2029d425b5b59c2659e04bda59a8c3696638351a
Johnny Lim <izeye@naver.com>
2023-04-27 22:25:27 +0900

    Support JavaScript eval()

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // `script` input defined in action metadata file
+    const script = core.getInput('script');
+    console.log(`script: ${script}`);
+
+    const evaluated = eval(script);
+    console.log(`evaluated: ${evaluated}`);
+
+    core.setOutput("stdout", evaluated);
 } catch (error) {
     core.setFailed(error.message);
 }
